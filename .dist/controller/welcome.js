01410814ef75f6d22e201143c3c2021c412b2d79
AbhijetPokhrel <abhijet.pokhrel@gmail.com>
2018-08-18 11:14:08 +0545

    gitignore finalized

--- .dist/controller/welcome.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-var index = exports.index = function index(knode, writer) {
-    writer.write(knode.view("views/welcome.html", { msg: { txt: "Hello !! " } }));
-};
